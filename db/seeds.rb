a0c84637406229629b1fdca37bab15f688bfdb1f
Pablo <paul.ladiv123@gmail.com>
2018-09-09 18:49:48 +0100

    edit seed file and run migration again

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@ pokerhands = Pokerhand.create(
                                   togglecount: 6 ,
                                   turnyellow: false,
                                   displayinstruction: "THE HAND IS FINISHED",
-                                  flopoop: "check_oop",
+                                  flopoop: "check_raise_oop",
                                   flopip: "check_ip",
-                                  turnoop: "bet_oop",
+                                  turnoop: "bet_call_oop",
                                   turnip: "call_ip",
-                                  riveroop: "check_ip",
-                                  rivrerip: "bet_ip"
+                                  riveroop: "bet_fold_oop",
+                                  rivrerip: "call_ip"
                               },
                               {
                                   sc1:"king_heart.png",
@@ -29,12 +29,12 @@ pokerhands = Pokerhand.create(
                                   togglecount: 6 ,
                                   turnyellow: false,
                                   displayinstruction: "THE HAND IS FINISHED",
-                                  flopoop: "check_oop",
-                                  flopip: "check_ip",
-                                  turnoop: "bet_oop",
+                                  flopoop: "check_fold_oop",
+                                  flopip: "bet_fold_ip",
+                                  turnoop: "bet_raise_oop",
                                   turnip: "call_ip",
-                                  riveroop: "check_ip",
-                                  rivrerip: "bet_ip"
+                                  riveroop: "check_raise_ip",
+                                  rivrerip: "bet_fold_ip"
                               },
                               {
                                   sc1:"queen_heart.png",
@@ -47,12 +47,12 @@ pokerhands = Pokerhand.create(
                                   togglecount: 6 ,
                                   turnyellow: false,
                                   displayinstruction: "THE HAND IS FINISHED",
-                                  flopoop: "bet_oop",
+                                  flopoop: "bet_fold_oop",
                                   flopip: "call_ip",
-                                  turnoop: "bet_oop",
+                                  turnoop: "bet_fold_oop",
                                   turnip: "call_ip",
-                                  riveroop: "check_ip",
-                                  rivrerip: "bet_ip"
+                                  riveroop: "bet_fold_oop",
+                                  rivrerip: "fold_ip"
                               }
                           ]
 )
