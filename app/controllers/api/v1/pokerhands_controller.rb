83020dc8b5c624a7694243d1c667708775c435c9
Pablo <paul.ladiv123@gmail.com>
2018-09-09 19:18:53 +0100

    update pokerhands controller to activate the post endpoint to be activated

--- app/controllers/api/v1/pokerhands_controller.rb
+++ app/controllers/api/v1/pokerhands_controller.rb
@@ -4,6 +4,18 @@ module Api::V1
       @pokerhands = Pokerhand.all
       render json: @pokerhands
     end
+
+    def create
+      @pokerhand = Pokerhand.create(pokerhand_params)
+      render json: @pokerhand
+    end
+
+    private
+
+    def  pokerhand_params
+      params.require(:pokerhand).permit(:sc1, :sc2, :sc3, :sc4, :sc4, :sc5, :gamestreet, :turntoplay, :togglecount, :turnyellow, :displayinstruction, :flopoop, :flopip, :turnoop, :turnip, :riveroop, :rivrerip)
+    end
+
   end
 end
 
